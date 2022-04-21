7c31668bcddd1649cda2d86dead4181904d6a0b1
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-21 00:54:47 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,40 @@
+/**
+ * THIS CLASS CONTAINS THE PIECE'S POSITION ON THE BOARD. 
+ */
+
+package boardgame;
+
+public class Position {
+
+	// POSITION IN THE MATRIX
+	private int row;
+	private int column;
+	
+	// CONSTRUCTOR
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	// GETTERS AND SETTERS
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
