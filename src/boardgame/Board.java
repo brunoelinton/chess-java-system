3fdeda899ef20aa9e7f5f8a59585358f4d931ceb
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-26 10:28:11 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	
 	// CONSTRUCTOR
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -18,30 +21,48 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
-	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
 	}	
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	// METHOD THAT PLACES A PIECE ON THE BOARD
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is a already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
+	// AUXILIARY METHOD TO CHECK A VALID POSITION ON THE BOARD
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row< rows && column >= 0 && column < columns;
+	}
+	
+	// METHOD TO CHECK A POSITION ON THE BOARD
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	// METHOD TO CHECK IF THERE IS A PIECE ON THE BOARD
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
+	}
 }
---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,12 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+
+	// ID SERIAL VERSION
+	private static final long serialVersionUID = 1L;
+
+	// CONSTRUCTOT
+	public BoardException(String msg) {
+		super(msg);
+	}
+}
